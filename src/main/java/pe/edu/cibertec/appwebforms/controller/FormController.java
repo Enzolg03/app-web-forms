0fdea3a4dfdf5866e384bdfa97bc5ab11eef62b0
EduardoMolinaC <clausmolina1999@gmail.com>
2023-09-17 11:01:42 -0500

    form with bootstrap

---
+++ src/main/java/pe/edu/cibertec/appwebforms/controller/FormController.java
@@ -0,0 +1,4 @@
+package pe.edu.cibertec.appwebforms.controller;
+
+public class FormController {
+}
---
+++ src/main/java/pe/edu/cibertec/appwebforms/controller/FormSumaController.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appwebforms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FormSumaController {
+    @GetMapping("/promedionotas")
+    public String index(){
+        return "formSuma";
+    }
+}
