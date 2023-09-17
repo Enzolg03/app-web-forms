8f05f545e2023e1d3b738cf383411fa9d3cd289f
EduardoMolinaC <clausmolina1999@gmail.com>
2023-09-17 09:34:36 -0500

    Primer commit :D

---
+++ src/main/java/pe/edu/cibertec/appwebforms/AppWebFormsApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appwebforms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebFormsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebFormsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/appwebforms/AppWebFormsApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appwebforms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebFormsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
