9d6808873a351e11b830ff2b346bae51e3520205
Enzolg03 <enzojoaquinlimaygallo@gmail.com>
2023-09-24 11:41:37 -0500

    2do commit

---
+++ src/main/java/pe/edu/cibertec/appwebforms/Model/PrimoModel.java
@@ -0,0 +1,15 @@
+package pe.edu.cibertec.appwebforms.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PrimoModel {
+    private Integer numero;
+
+}
